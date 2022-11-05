1e7535b2529a22d70a3c301892d533d71e6438c7
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2022-11-05 19:56:29 +0530

    If-else Task

---
+++ Task1.java
@@ -0,0 +1,21 @@
+class Task1
+{
+public static void main(String[] args)
+{
+String movieName="Check Movie Review and Movie Subtitle Tamil";
+boolean isMovieReviewGood = true;
+boolean isMovieSubTitleTamil = true;
+
+if(isMovieReviewGood == true )
+{
+    if(isMovieSubTitleTamil == false)
+    {
+    System.out.println("Book Movie Ticket");
+    }
+    else {  
+        System.out.println("Not Book Movie Ticket");
+        }
+        }
+
+}
+}
